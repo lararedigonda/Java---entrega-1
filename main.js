f276c013fd6237c26393d0c558c4130befddac33
Lara Redigonda <redigondalara@gmail.com>
2026-01-25 22:48:19 -0300

    eliminacion de comentado

--- main.js
+++ main.js
@@ -1,55 +1,3 @@
-// // let saludar = true
-// // if (saludar) {
-// //     console.log ("Bienvenida a nuestros cursos")
-// // }
-
-// // const cursos = ["Desayuno y cerámica", "Merienda y cerámica", "Vino y cerámica"]
-
-// // // Ingresar edad
-
-// // let edad = parseInt(prompt("Ingrese su edad"))
-
-// // // condicional
-// // if (edad >= 18) {
-
-// //     let opcion = parseInt(prompt("Podes inscribirte en los siguientes cursos:\n" + "1-" + "Desayuno y cerámica" + "\n" + "2-" + "Merienda y cerámica" + "\n" + "3-" + "Vino y cerámica" + "\n" + "Ingresa el número del curso que desees"))
-// //     switch (opcion) {
-// //         case 1: 
-// //         alert ("Desayuno y cerámica 7/2 10:00 hs")
-// //         break
-// //         case 2: 
-// //         alert ("Merienda y cerámica 17/2 16:00 hs")
-// //         break
-// //         case 3: 
-// //         alert ("Vino y cerámica 7/2 19:00 hs")
-// //         break
-// //         default: 
-// //         alert ("Opción incorrecta")
-// //         }
-
-// //     let confirmacion = prompt("Confirmas la inscripcion? si/no")
-// //         if (confirmacion == "si") {
-// //         let inscripcion = prompt("ingresa nombre y apellido")
-// //         alert("Te inscribiste en: " + cursos[opcion - 1])
-// //     }}
-// //     else {
-// //         let opcion = parseInt(prompt("Podes inscribirte en los siguientes cursos:\n" + "1-" + "Desayuno y cerámica" + "\n" + "2-" + "Merienda y cerámica" + "\n"+ "Ingresa el número del curso que desees"))
-// //         switch (opcion) {
-// //         case 1: 
-// //         alert ("Desayuno y cerámica 7/2 10:00 hs")
-// //         break
-// //         case 2: 
-// //         alert ("Merienda y cerámica 17/2 16:00 hs")
-// //         break
-// //         default: 
-// //         alert ("Opción incorrecta")
-// //         }
-    
-// //         let confirmacion = prompt("Confirmas la inscripcion? si/no")
-// //     if (confirmacion == "si") {
-// //         let inscripcion = prompt("ingresa nombre y apellido")
-// //         alert("Te inscribiste en: " + cursos[opcion - 1])
-// //     }}
 
 let saludar = true
 if (saludar) {
@@ -58,9 +6,10 @@ if (saludar) {
 
 const cursos = ["Desayuno y cerámica", "Merienda y cerámica", "Vino y cerámica"]
 
-// Ingresar edad
+// // Ingresar edad
 let edad = parseInt(prompt("Ingrese su edad"))
 
+// Elección cursos
 function elegirCurso(edad) {
     let opcion = 0
 
@@ -101,13 +50,13 @@ function elegirCurso(edad) {
 }
 
 function confirmacion(opcion, cursos) {
-    let confirmacionCurso = prompt("¿Confirmas la inscripcian? si/no").toLowerCase ()
+    let confirmacionCurso = prompt("¿Confirmas la inscripcion? si/no").toLowerCase ()
 
     if (confirmacionCurso == "si") {
-        let nombre = prompt("Ingresá nombre y apellido")
+        let nombre = prompt("Ingresa nombre y apellido")
         alert("Te inscribiste en:\n" + cursos[opcion - 1])
     } else {
-        alert("Inscripción cancelada")
+        alert("Inscripcion cancelada")
     }
 }
 
